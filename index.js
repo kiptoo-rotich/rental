f9141d17d6a44cd7fb24257434fb1d9fe68b7de2
Softonic254 <rotichenoch27@gmail.com>
2021-03-27 17:12:45 +0300

    Adding user guide

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ function calculate() {
 
     function totalAmount() {
         let totalAmount = (occupied_rooms * room_value) - monthly_expenses;
-        if (monthly_expenses != 50000) {
-            let explanation = prompt("Tell us why there is a deviation in monthly expenditure.");
-            document.getElementById("explanation").innerHTML = "&#10004 Deviation from normal expenditure is due to " + explanation;
-        }
-        if (totalAmount < 0) {
-            document.getElementById("expected_amount").innerHTML = "&#10004 You made a loss of Kshs. " + totalAmount + ".";
+        if (monthly_expenses < 0) {
+            alert("Expanses cannot be negative in value.");
         } else {
-            document.getElementById("expected_amount").innerHTML = "&#10004 The expected amount of collected money is<br> Kshs. " + totalAmount + " less Kshs." + monthly_expenses + " expenditure.";
+            if (monthly_expenses != 50000) {
+                let explanation = prompt("Tell us why there is a deviation in monthly expenditure.");
+                document.getElementById("explanation").innerHTML = "&#10004 Deviation from normal expenditure is due to " + explanation;
+            }
+            if (totalAmount < 0) {
+                document.getElementById("expected_amount").innerHTML = "&#10071 You made a loss of Kshs. " + totalAmount * -1 + " &#128542.";
+            } else {
+                if (monthly_expenses < 0) {
+                    alert("Expenses cannot be negative in value.")
+                } else if (monthly_expenses == 0) {
+                    document.getElementById("expected_amount").innerHTML = "&#10004 The expected amount of rental income is<br> Kshs. " + totalAmount + " with NO expenditure.";
+                } else {
+                    document.getElementById("expected_amount").innerHTML = "&#10004 The expected amount of rental income is<br> Kshs. " + totalAmount + " less Kshs. " + monthly_expenses + " expenditure.";
+                }
+            }
         }
     }
 
@@ -22,7 +32,7 @@ function calculate() {
         let vacant = (total_rooms - occupied_rooms);
         if (vacant < 0 || vacant > total_rooms) {
             document.getElementById("header").innerHTML = "Results";
-            document.getElementById("Number_of_vacant_rooms").innerHTML = "&#10004 You have entered wrong number of occupied room.<br> Kindly re-check."
+            document.getElementById("Number_of_vacant_rooms").innerHTML = "&#10060 You have entered wrong number of occupied room.<br> Kindly re-check."
         } else if (vacant == 0) {
             document.getElementById("header").innerHTML = "Results";
             document.getElementById("Number_of_vacant_rooms").innerHTML = "&#10004 All rooms are occupied.";
@@ -40,6 +50,10 @@ function calculate() {
         alert("Tell me the number of occupied rooms.")
     }
 
+}
+
+function guide() {
+    document.getElementById("guide_text").innerHTML = "In order to know the expected amount of income, you'll need to enter the number of occupied rooms. Before you submit, ensure the other details are correct. If NOT, you can change the value. Changing the value of expenditure will need an explanation to be recorded. You can also change the number of rooms."
 }
 
 function refresh() {
